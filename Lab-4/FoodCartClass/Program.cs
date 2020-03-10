a7d9f1a0106cd1614b1b341a116a57d370483d36
Manuel Larios <mlarios12@wou.edu>
2020-03-10 10:37:42 -0700

    Created C# project

---
+++ Lab-4/FoodCartClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodCartClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
